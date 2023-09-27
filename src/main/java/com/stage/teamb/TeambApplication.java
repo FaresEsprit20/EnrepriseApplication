dd7ae8e02cb6ddecbf935a17114b7b98669af768
FaresEsprit20 <=>
2023-09-27 18:39:05 +0200

    first commit

--- src/main/java/com/stage/teamb/TeambApplication.java
+++ src/main/java/com/stage/teamb/TeambApplication.java
@@ -3,7 +3,7 @@ package com.stage.teamb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication()
+@SpringBootApplication
 public class TeambApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TeambApplication.class, args);
