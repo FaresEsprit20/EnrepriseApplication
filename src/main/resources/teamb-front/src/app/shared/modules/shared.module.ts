d4e2ee29c8975c602aada4671f9d6815bfde440f
FaresEsprit20 <=>
2023-12-24 03:53:50 +0100

    spring security

---
+++ src/main/resources/teamb-front/src/app/shared/modules/shared.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MaterialModule } from './material/material.module';
+import { SortPipe } from '../../pipes/sort.pipe';
+import { DownvoteComponent } from '../ui/downvote/downvote.component';
+import { SearchComponent } from '../ui/search/search.component';
+import { UpvoteComponent } from '../ui/upvote/upvote.component';
+import { AlertComponent } from './custom-ui/alerts/alert.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { SortingComponent } from '../ui/sort/sort.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    SortPipe,
+    SortingComponent,
+    UpvoteComponent,
+    DownvoteComponent,
+    SearchComponent,
+    AlertComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    HttpClientModule
+  ],
+  exports: [
+    SortPipe,
+    SortingComponent,
+    UpvoteComponent,
+    DownvoteComponent,
+    SearchComponent,
+    AlertComponent
+  ]
+})
+export class SharedModule { }
