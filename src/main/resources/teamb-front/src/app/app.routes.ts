b2f11a12142ee9da69d362abf4a3760f3ed8a568
FaresEsprit20 <=>
2024-01-01 03:45:43 +0100

    spring security

--- src/main/resources/teamb-front/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-
-export const routes: Routes = [
-    { path: 'authentication', loadChildren: () => import('./core/core.module').then((m) => m.CoreModule) },
-    { path: 'employees', loadChildren: () => import('./components/employees-management/employees-management.module').then((m) => m.EmployeesManagementModule) },
-    { path: 'responsibles', loadChildren: () => import('./components/responsibles-management/responsibles-management.module').then((m) => m.ResponsiblesManagementModule) }, 
-    { path: '', redirectTo: '/authentication/login', pathMatch: 'full' }
-];
