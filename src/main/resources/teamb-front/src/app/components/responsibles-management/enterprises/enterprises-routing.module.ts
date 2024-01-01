919b806698c23439d69fbd9d55a4567807aad753
FaresEsprit20 <=>
2024-01-01 14:37:33 +0100

    spring security

--- src/main/resources/teamb-front/src/app/components/responsibles-management/enterprises/enterprises-routing.module.ts
+++ src/main/resources/teamb-front/src/app/components/responsibles-management/enterprises/enterprises-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { EnterpriseListComponent } from './enterprise-list/enterprise-list.component';
 import { CreateEnterpriseComponent } from './create-enterprise/create-enterprise.component';
 import { EnterpriseDetailsComponent } from './enterprise-details/enterprise-details.component';
-import { UpdateDepartmentComponent } from '../departments/update-department/update-department.component';
+import { UpdateEnterpriseComponent } from './update-enterprise/update-enterprise.component';
 
 
 const routes: Routes = [
   { path: 'list', component: EnterpriseListComponent },
   { path: 'create', component:  CreateEnterpriseComponent},
   { path: 'details/:id', component:  EnterpriseDetailsComponent},
-  { path: 'details/:id/update', component:  UpdateDepartmentComponent},
+  { path: 'details/:id/update', component:  UpdateEnterpriseComponent},
 ];
 
 @NgModule({
--- src/main/resources/teamb-front/src/app/components/responsibles-management/responsibles-management-routing.module.ts
+++ src/main/resources/teamb-front/src/app/components/responsibles-management/responsibles-management-routing.module.ts
@@ -3,7 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: 'enterprises', loadChildren: () => import('./enterprises/enterprises.module').then((m) => m.EnterprisesModule) },
-  { path: 'departments', loadChildren: () => import('./departments/departments.module').then((m) => m.DepartmentsModule) },
   { path: 'management/employees', loadChildren: () => import('./employees/employees.module').then((m) => m.EmployeesModule) },
 
 ];
